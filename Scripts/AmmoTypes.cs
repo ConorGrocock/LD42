d5943c114dac7d18f5a38b755542a77faabda4d1
Unknown <rb2750@live.co.uk>
2018-08-13 17:54:29 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Scripts/AmmoTypes.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoTypes : MonoBehaviour
+{
+	public GameController gc;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gc.player.on
+	}
+}
